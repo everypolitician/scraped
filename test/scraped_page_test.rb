985e634e597d26918263624dc43037737f4c8d9a
Chris Mytton <chrismytton@users.noreply.github.com>
2016-10-28 18:07:10 +0100

    Add a failing test for ScrapedPage

--- test/scraped_page_test.rb
+++ test/scraped_page_test.rb
@@ -5,9 +5,19 @@ describe ScrapedPage do
     ::ScrapedPage::VERSION.wont_be_nil
   end
 
+  class TestPage < ScrapedPage
+    field :foo do
+      noko.css('h1')
+    end
+  end
+
+  class TestStrategy < ScrapedPage::Strategy
+  end
+
   describe 'a simple scraper' do
     it 'serializes to a hash' do
-      ScrapedPage.new.to_h.must_equal({})
+      page = TestPage.new('http://example.com', strategy: TestStrategy)
+      page.to_h.must_equal foo: 'bar'
     end
   end
 end
