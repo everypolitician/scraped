b11fae96019795d777f73675997ad286fd0b2ccc
Chris Mytton <chrismytton@users.noreply.github.com>
2016-10-28 17:52:48 +0100

    Switch tests to describe syntax

--- test/scraped_page_test.rb
+++ test/scraped_page_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
-class ScrapedPageTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::ScrapedPage::VERSION
+describe ScrapedPage do
+  it 'has a version number' do
+    ::ScrapedPage::VERSION.wont_be_nil
   end
 end
