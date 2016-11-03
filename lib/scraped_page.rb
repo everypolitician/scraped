60ab4449b45c5bae1a0e723bc20ba3e457b35026
Chris Mytton <chrismytton@gmail.com>
2016-11-03 18:00:13 +0100

    Initial implementation of ScrapedPage class
    This implementation was ported over from the prototypes that I did in
    the `guernsey-2016` and `mongolia-khurai-wp-multiple-terms` scrapers.
    
    At the moment I'm just dumping it all in one big file, but it would be
    good to split this into separate files sooner rather than later.

--- lib/scraped_page.rb
+++ lib/scraped_page.rb
@@ -1,5 +1,74 @@
+# frozen_string_literal: true
 require 'scraped_page/version'
 
-module ScrapedPage
-  # Your code goes here...
+require 'nokogiri'
+require 'open-uri'
+require 'field_serializer'
+require 'scraped_page_archive'
+
+# Abstract class which scrapers can extend to implement their functionality.
+class ScrapedPage
+  include FieldSerializer
+
+  class Response
+    attr_reader :status, :headers, :body
+
+    def initialize(body:, status: 200, headers: {})
+      @status = status
+      @headers = headers
+      @body = body
+    end
+  end
+
+  module Strategy
+    class LiveRequest
+      def response(url)
+        log "Fetching #{url}"
+        response = open(url)
+        {
+          status:  response.status.first.to_i,
+          headers: response.meta,
+          body:    response.read,
+        }
+      end
+
+      private
+
+      def log(message)
+        warn "[#{self.class}] #{message}"
+      end
+    end
+
+    class LiveRequestArchive < LiveRequest
+      def response(url)
+        log "Archiving #{url}"
+        scraped_page_archive.record { super }
+      end
+
+      private
+
+      def scraped_page_archive
+        @scraped_page_archive ||= ScrapedPageArchive.new(
+          ScrapedPageArchive::GitStorage.new
+        )
+      end
+    end
+  end
+
+  def initialize(url:, strategy: Strategy::LiveRequest.new)
+    @url = url
+    @strategy = strategy
+  end
+
+  private
+
+  attr_reader :url, :strategy
+
+  def noko
+    @noko ||= Nokogiri::HTML(response.body)
+  end
+
+  def response
+    @response ||= Response.new(strategy.response(url))
+  end
 end
