a263b2777702440ad249f44e6780b5204fbf9249
Chris Mytton <chrismytton@gmail.com>
2016-11-16 10:12:04 +0100

    Add require_all gem
    This makes it much easier to require all of the code in `lib` that the
    gem uses, rather than having to have a require statement for each line.

--- lib/scraped_page.rb
+++ lib/scraped_page.rb
@@ -1,11 +1,8 @@
 # frozen_string_literal: true
-require 'scraped_page/version'
-require 'scraped_page/strategy/live_request'
-require 'scraped_page/strategy/live_request_archive'
-require 'scraped_page/request'
-require 'scraped_page/response'
 require 'nokogiri'
 require 'field_serializer'
+require 'require_all'
+require_rel 'scraped_page'
 
 # Abstract class which scrapers can extend to implement their functionality.
 class ScrapedPage
--- scraped_page.gemspec
+++ scraped_page.gemspec
@@ -22,6 +22,7 @@ Gem::Specification.new do |spec|
   spec.add_runtime_dependency 'nokogiri'
   spec.add_runtime_dependency 'field_serializer'
   spec.add_runtime_dependency 'scraped_page_archive', '>= 0.5'
+  spec.add_runtime_dependency 'require_all'
 
   spec.add_development_dependency 'bundler', '~> 1.13'
   spec.add_development_dependency 'rake', '~> 10.0'
