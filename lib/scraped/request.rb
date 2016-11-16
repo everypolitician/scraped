eff1e8a8fa12df496a43f608e7edb8bbd828d563
Chris Mytton <chrismytton@gmail.com>
2016-11-16 14:50:20 +0100

    Move response processors to the response method
    This adds a `processors` argument to `Request#response` which can
    provide a list of objects that respond to `call` and take a response,
    process it and optionally return a new response.

--- lib/scraped/request.rb
+++ lib/scraped/request.rb
@@ -3,16 +3,15 @@ require 'scraped/response'
 
 class Scraped
   class Request
-    def initialize(url:, readers: [Strategy::LiveRequest.new], writers: [])
+    def initialize(url:, readers: [Strategy::LiveRequest.new])
       @url = url
       @readers = readers
-      @writers = writers
     end
 
-    def response
+    def response(processors = [])
       abort "Failed to fetch #{url}" if first_successful_response.nil?
       response = Response.new(first_successful_response.merge(url: url))
-      writers.each { |w| w.save_response(response) }
+      processors.reduce(response) { |r, handler| handler.call(r) }
       response
     end
 
@@ -23,6 +22,6 @@ class Scraped
         readers.lazy.map { |r| r.response(url) }.reject(&:nil?).first
     end
 
-    attr_reader :url, :readers, :writers
+    attr_reader :url, :readers
   end
 end
