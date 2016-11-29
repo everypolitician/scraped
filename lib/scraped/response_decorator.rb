b9f84d3e17ea24b60469be606a2aefbc98a7cab3
Chris Mytton <chrismytton@gmail.com>
2016-11-29 17:11:25 +0100

    Move response decorating logic to its own class
    This means we can reuse this logic at the individual class level for
    decorating those responses.

---
+++ lib/scraped/response_decorator.rb
@@ -0,0 +1,23 @@
+class Scraped
+  class ResponseDecorator
+    def initialize(response:, decorators:)
+      @original_response = response
+      @decorators = decorators.to_a
+    end
+
+    def response
+      decorators.reduce(original_response) do |r, decorator_config|
+        unless decorator_config.respond_to?(:delete)
+          decorator_config = { decorator: decorator_config }
+        end
+        decorator_class = decorator_config.delete(:decorator)
+        decorator_class.new(response: r, config: decorator_config)
+                       .decorated_response
+      end
+    end
+
+    private
+
+    attr_reader :original_response, :decorators
+  end
+end
