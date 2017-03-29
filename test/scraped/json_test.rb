6fbfb7e47e36b207c5dd638dcf9544f075b81d1f
Oliver Denman <oliver@mysociety.org>
2017-03-29 10:07:42 +0100

    fixup! Add Scraped::JSON

--- test/scraped/json_test.rb
+++ test/scraped/json_test.rb
@@ -1,53 +1,51 @@
 require 'test_helper'
 
 describe Scraped::JSON do
-  describe 'accessing JSON via nokogiri' do
-    let(:json) do
-      <<-JSON
-        {
-          "test_content": "Hi there!",
-          "member_info": {
-            "name": "Alice"
-          }
+  let(:json) do
+    <<-JSON
+      {
+        "test_content": "Hi there!",
+        "member_info": {
+          "name": "Alice"
         }
-      JSON
+      }
+    JSON
+  end
+
+  let(:response) do
+    Scraped::Response.new(
+      body: json,
+      url:  'http://example.com'
+    )
+  end
+
+  class JSONMemberInfo < Scraped::JSON
+    field :name do
+      json[:name]
+    end
+  end
+
+  class JSONExamplePage < Scraped::JSON
+    field :content do
+      json[:test_content]
     end
 
-    let(:response) do
-      Scraped::Response.new(
-        body: json,
-        url:  'http://example.com'
-      )
+    field :member do
+      fragment json[:member_info] => JSONMemberInfo
     end
+  end
 
-    class JSONMemberInfo < Scraped::JSON
-      field :name do
-        json[:name]
-      end
-    end
+  let(:page) { JSONExamplePage.new(response: response) }
 
-    class JSONExamplePage < Scraped::JSON
-      field :content do
-        json[:test_content]
-      end
+  it 'returns the expected content' do
+    page.content.must_equal 'Hi there!'
+  end
 
-      field :member do
-        fragment json[:member_info] => JSONMemberInfo
-      end
-    end
-
+  describe 'fragment method' do
     let(:page) { JSONExamplePage.new(response: response) }
 
-    it 'returns the expected content' do
-      page.content.must_equal 'Hi there!'
-    end
-
-    describe 'fragment method' do
-      let(:page) { JSONExamplePage.new(response: response) }
-
-      it 'returns content from a subset of the page' do
-        page.member.name.must_equal 'Alice'
-      end
+    it 'returns content from a subset of the page' do
+      page.member.name.must_equal 'Alice'
     end
   end
 end
